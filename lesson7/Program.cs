6302c42ea45a3bcb71a36e666663399f7a3a019d
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-13 17:47:35 +0300

    Add Test class

--- lesson7/Program.cs
+++ lesson7/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            
+            Show_Cars();
+        }
+
+        public static void Show_Cars()
+        {
+            int choice;
+            Automobile car;
+            do
+            {
+                Console.Write("Enter which car to show(BMW - 1, Audi - 2, Hyundai - 3): ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        car = new BMW_X5();
+                        Console.WriteLine($"Car's name: {car.name}");
+                        Console.WriteLine($"Car's mark: {car.mark}");
+                        Console.WriteLine($"Car's type: {car.type}");
+                        Console.WriteLine($"Car's engine capacity: {car.engine_capacity}");
+                        break;
+                    case 2:
+                        car = new Audi_A8();
+                        Console.WriteLine($"Car's name: {car.name}");
+                        Console.WriteLine($"Car's mark: {car.mark}");
+                        Console.WriteLine($"Car's type: {car.type}");
+                        Console.WriteLine($"Car's engine capacity: {car.engine_capacity}");
+                        break;
+                    case 3:
+                        car = new Hyundai_Solaris();
+                        Console.WriteLine($"Car's name: {car.name}");
+                        Console.WriteLine($"Car's mark: {car.mark}");
+                        Console.WriteLine($"Car's type: {car.type}");
+                        Console.WriteLine($"Car's engine capacity: {car.engine_capacity}");
+                        break;
+                }
+                Console.WriteLine();
+            } while (choice != 0);
         }
     }
 }
---
+++ lesson7/Test.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson7
+{
+    internal class Test
+    {
+    }
+
+    class Automobile
+    {
+        public string name { get; set; }
+        public string mark { get; set; }
+        public string type { get; set; }
+        public double engine_capacity { get; set; }
+
+        public Automobile()
+        {
+            this.name = "None";
+            this.mark = "None";
+            this.type = "None";
+            this.engine_capacity = 0;
+        }
+
+        public Automobile(string name, string mark, string type, double engine_capacity)
+        {
+            this.name = name;
+            this.mark = mark;
+            this.type = type;
+            this.engine_capacity = engine_capacity;
+        }
+    }
+
+    class BMW_X5 : Automobile
+    {
+        public BMW_X5()
+        {
+            this.name = "BMW X5M60i Sports";
+            this.mark = "BMW";
+            this.type = "Sport car";
+            this.engine_capacity = 4.4;
+        }
+    }
+
+    class Audi_A8 : Automobile
+    {
+        public Audi_A8()
+        {
+            this.name = "Audi A8 4.2 FSI quattro Tiptronic";
+            this.mark = "Audi";
+            this.type = "Sedan";
+            this.engine_capacity = 4.2;
+        }
+    }
+
+    class Hyundai_Solaris : Automobile
+    {
+        public Hyundai_Solaris()
+        {
+            this.name = "Hyundai Solaris 2020 2-Generation";
+            this.mark = "Hyundai";
+            this.type = "Sedan";
+            this.engine_capacity = 1.6;
+        }
+    }
+}
